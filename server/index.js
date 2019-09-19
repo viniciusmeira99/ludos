e7e86c1bd430279d5fc8f7a24f216d61c88a2e76
vinicius <viniciusmeira99@hotmail.com>
2019-09-19 18:18:07 -0300

    Corrigindo diretorio do react

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, '../build')));
 app.use('/api', router);
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 if (module === require.main) {
